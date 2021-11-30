509494576c0f4983d95ae7442971285baad2608d
OrganicAC <anthonychaplin17@gmail.com>
2021-11-30 12:39:59 +0000

    Further Refactoring

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
-students = [
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Darth Vader", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Kreuger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+
+  students = []
+
+  name = gets.chomp
+
+  #while name is NOT empty, repeat code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+#adds the student to array via hash, then gets another name
+    name = gets.chomp
+  end
+
+  students
+end
 
 def print_header
     puts "The students of Villains Academy"
@@ -28,7 +33,7 @@ puts "Overall, we have #{names.count} great students."
 end
 
 ###
-
+students = input_students
 print_header
 print(students)
 print_footer(students)
