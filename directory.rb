57bcd698cbf56ee9130b2765775e78c7571b717e
OrganicAC <anthonychaplin17@gmail.com>
2021-12-01 15:53:47 +0000

    Step 13

--- directory.rb
+++ directory.rb
@@ -1,24 +1,4 @@
-@students = []
-
-def input_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  @students
-  name = gets.chomp
-  while !name.empty? do
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} students"
-#adds the student to array via hash, then gets another name
-    name = gets.chomp
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end
+@students = [] 
 
 def print_menu
   puts "1. Input the students"
@@ -28,61 +8,92 @@ def print_menu
   puts "9. Exit"
 end
 
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "3"
+    save_students
+  when "4"
+    load_students
+  when "9"
+    exit
+  else
+    puts "I don't know what you meant, try again"
+  end
+end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  name = STDIN.gets.chomp
+  while !name.empty? do
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
+    name = STDIN.gets.chomp
+  end
+end
+
 def show_students
   print_header
   print_student_list
   print_footer
 end
 
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print_student_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
+end
+
 def save_students
   file = File.open("students.csv", "w")
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(", ")
+    csv_line = student_data.join(",")
     file.puts csv_line
   end
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
-  name, cohort, = line.chomp.split(",")
+  name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
 
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil? 
+  if File.exists?(filename) 
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else 
+    puts "Sorry, #{filename} doesn't exist."
+    exit
   end
 end
 
-def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
-end
-
-def print_student_list
-  @students.each do |student|
-    puts "#{student[:name]}, (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer
-puts "Overall, we have #{@students.count} great students."
-end
-
+try_load_students
 interactive_menu
